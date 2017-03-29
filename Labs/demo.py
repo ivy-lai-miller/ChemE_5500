ecbfde1309645d53bb6ca0ad7d756a932f5fc231
ivy-lai-miller <il227@cornell.edu>
2017-03-29 15:40:36 -0400

    Added Lab 6 graph function

---
+++ Labs/demo.py
@@ -0,0 +1,10 @@
+from graph import Graph
+
+def f2(x):
+    return x**2
+
+g1 = Graph(f2,20,0.1)
+g1.plot("demo1")
+
+g2 = Graph()
+g2.plot("demo2")
---
+++ Labs/graph.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Graph:
+
+    def f(x):
+        return x
+
+    def __init__(self, function= lambda x: x, end_points=10,step_size = 1):
+        self.end_points = end_points
+        self.function = function
+        self.step_size = step_size
+
+    def plot(self,fname="Figure"):
+        # counter = 0
+        xval = []
+        xtemp = 0
+        yval = []
+        while xtemp < self.end_points:
+            yval.append(self.function(xtemp))
+            xval.append(xtemp)
+            xtemp += self.step_size
+
+        plt.plot(xval,yval)
+        fig = plt.gcf()
+        plt.show()
+
+        fig.savefig("%s.png" %fname)
